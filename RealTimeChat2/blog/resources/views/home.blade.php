5204d40f5891c8a35c313dd451dd0790ef349cce
AhmedFouad60 <lionmoon60@yahoo.com>
2018-04-29 05:05:18 +0200

    vue,laravel,webpack ,realtime chat

---
+++ RealTimeChat2/blog/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+
+        <div class="col-md-12">
+            <div class="row">
+                @include('includes.chatDashboard')
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+
